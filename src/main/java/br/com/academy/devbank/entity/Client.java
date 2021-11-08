b881c3058be2367c0993667d967f45abb6fb6121
leoracejsa <leoracejsa@gmail.com>
2021-11-08 13:02:36 -0300

    Ajuste no Client

--- src/main/java/br/com/academy/devbank/entity/Client.java
+++ src/main/java/br/com/academy/devbank/entity/Client.java
@@ -2,19 +2,16 @@ package br.com.academy.devbank.entity;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -47,7 +44,8 @@ public class Client implements Serializable {
 	@Column(name = "PASSWORD")
 	private String password;
 	
-	@OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+	@OneToMany(targetEntity = Account.class, cascade = CascadeType.ALL)
+	@JoinColumn(name = "IDCLIENT", referencedColumnName = "IDCLIENT")
 	private List<Account> account;
 
 }
